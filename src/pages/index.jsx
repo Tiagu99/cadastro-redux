3cb02a3659cf60e527d7a73c2dea995b0f4eab36
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-27 19:43:05 -0300

    cria index em pages para exportar as pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Cliente } from './Cliente';
+export { default as Notfound } from './Notfound';
+export { default as Login } from './Login';
+export { default as Dashboard } from './Dashboard';
