2a286397dce705affdd087d09a7b6594362cef3b
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-07-27 19:47:04 -0300

    implementa hostory no app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Home, Login, Cliente, Notfound } from './pages';
+import { Router, Route, Switch } from 'react-router-dom';
+import { Home, Login, Cliente, Dashboard, Notfound } from './pages/index.jsx';
+import history from './history.js';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={ history }>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/dashboard" component={Cliente} />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/register" component={Cliente} />
         <Route exact path="*" component={Notfound} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
